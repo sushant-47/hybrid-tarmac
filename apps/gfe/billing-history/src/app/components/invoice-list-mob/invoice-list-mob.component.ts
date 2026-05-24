2e4a521eb7b9d4cf2051aaea9589abe39b369901
SUSHANT KAKROO <37952551+sushant-47@users.noreply.github.com>
2026-05-24 12:45:01 +0000

    feat(billing-history): Example using luxon;

--- apps/gfe/billing-history/src/app/components/invoice-list-mob/invoice-list-mob.component.ts
+++ apps/gfe/billing-history/src/app/components/invoice-list-mob/invoice-list-mob.component.ts
@@ -16,8 +16,8 @@ export class InvoiceListMobile {
             next: console.log
         });
 
-        // const localDate = this._dateTimeService.convertToCustomFormat("25-12-2026", "Europe/London", "D MMM, YYYY", "America/Los_Angeles");
-        // console.log(localDate); // "Dec 25, 2026"
+        // Using fixed offset time zones
+        const localDate = this._dateTimeService.convertToCustomFormat("25-12-2026 05:00 AM", "MMM d, yyyy hh:mm a", "UTC+5:30", "UTC+0");
+        console.log(localDate); // "Dec 24, 2026 11:30 PM"
     }
-
 }
